44c2bb3e9736b93021414e25fcfb2b429cfb0a83
Humza Ali <humzaejazali@gmail.com>
2025-05-01 20:00:24 +0100

    added labels for video length

--- PrismPlayer.py
+++ PrismPlayer.py
@@ -1,8 +1,9 @@
 import sys
 import vlc
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QWidget, QVBoxLayout, QHBoxLayout, QSlider
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QWidget, QVBoxLayout, QHBoxLayout, QSlider, QLabel
 from PyQt5.QtMultimediaWidgets import QVideoWidget
+
 
 
 class VideoPlayer(QMainWindow):
@@ -21,6 +22,12 @@ class VideoPlayer(QMainWindow):
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.update_seekbar)
 
+    def format_time(self, ms):
+            seconds = int(ms / 1000)
+            minutes = seconds // 60
+            seconds = seconds % 60
+            return f"{minutes:02}:{seconds:02}"
+
 
     def init_ui(self):
         #used for displaying video inside the window
@@ -28,6 +35,10 @@ class VideoPlayer(QMainWindow):
         self.setCentralWidget(central_widget)
         self.video_widget = QVideoWidget(self)
         self.media_player.set_hwnd(int(self.video_widget.winId()))
+
+        # labels
+        self.current_time_label = QLabel("00:00", self)
+        self.total_time_label = QLabel("00:00", self) 
 
         # Buttons
         self.play_button = QPushButton("Play", self)
@@ -54,14 +65,18 @@ class VideoPlayer(QMainWindow):
         self.seek_bar = QSlider(Qt.Horizontal, self)
         self.seek_bar.setRange(0, 1000)
         self.seek_bar.sliderMoved.connect(self.set_position)
+
+        seek_layout = QHBoxLayout()
+        seek_layout.addWidget(self.current_time_label)
+        seek_layout.addWidget(self.seek_bar)
+        seek_layout.addWidget(self.total_time_label)
        
         #draws the black box for video and the seek bar
         main_layout = QVBoxLayout()
-        main_layout.addWidget(self.video_widget)
-        main_layout.addWidget(self.seek_bar)
+        main_layout.addWidget(self.video_widget, stretch=1)
+        main_layout.addLayout(seek_layout)
         main_layout.addLayout(control_layout)
         central_widget.setLayout(main_layout)
-
 
     def play_video(self):
         self.media_player.play()
@@ -92,8 +107,13 @@ class VideoPlayer(QMainWindow):
 
     def update_seekbar(self):
         if self.media_player.is_playing():
-            pos = self.media_player.get_position()  # 0.0 to 1.0
+            pos = self.media_player.get_position()
+            length = self.media_player.get_length()  # in ms
+            current_time = int(pos * length)
+
             self.seek_bar.setValue(int(pos * 1000))
+            self.current_time_label.setText(self.format_time(current_time))
+            self.total_time_label.setText(self.format_time(length))
 
     def set_position(self, value):
         self.media_player.set_position(value / 1000.0)
