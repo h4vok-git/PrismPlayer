dcf00e5c1f6049f94b6aa6b1bc19325476ae5e45
Humza Ali <humzaejazali@gmail.com>
2025-05-01 18:23:37 +0100

    added buttons and video plays inside program now

--- PrismPlayer.py
+++ PrismPlayer.py
@@ -1,20 +1,60 @@
 import sys
 import vlc
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 
 
 class VideoPlayer(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PrismPlayer")
-        self.setGeometry(100, 100, 300, 100)
-        # this creates the VLC instance  
+        self.setGeometry(100, 100, 800, 600)
+
+        # Create a VLC instance
         self.vlc_instance = vlc.Instance()
         self.media_player = self.vlc_instance.media_player_new()
-        
-        open_button = QPushButton("Open Video", self)
-        open_button.clicked.connect(self.open_file)
-        open_button.setGeometry(100, 40, 100, 40)  
+        self.init_ui()
+
+    def init_ui(self):
+        #used for displaying video inside the window
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        self.video_widget = QVideoWidget(self)
+        self.media_player.set_hwnd(int(self.video_widget.winId()))
+
+        # Buttons
+        self.play_button = QPushButton("Play", self)
+        self.pause_button = QPushButton("Pause", self)
+        self.stop_button = QPushButton("Stop", self)
+        self.open_button = QPushButton("Open", self)
+
+        self.play_button.clicked.connect(self.play_video)
+        self.pause_button.clicked.connect(self.pause_video)
+        self.stop_button.clicked.connect(self.stop_video)
+        self.open_button.clicked.connect(self.open_file)
+
+        # draws buttons  
+        control_layout = QHBoxLayout()
+        control_layout.addWidget(self.open_button)
+        control_layout.addWidget(self.play_button)
+        control_layout.addWidget(self.pause_button)
+        control_layout.addWidget(self.stop_button)
+       
+        #draws the black box for video
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.video_widget)
+        main_layout.addLayout(control_layout)
+        central_widget.setLayout(main_layout)
+
+    def play_video(self):
+        self.media_player.play()
+
+    def pause_video(self):
+        self.media_player.pause()
+
+    def stop_video(self):
+        self.media_player.stop()
 
     def open_file(self):
         file_path, _ = QFileDialog.getOpenFileName(
@@ -23,7 +63,8 @@ class VideoPlayer(QMainWindow):
         if file_path:
             media = self.vlc_instance.media_new(file_path)
             self.media_player.set_media(media)
-            self.media_player.play()
+            self.media_player.set_hwnd(int(self.video_widget.winId()))
+            self.play_video()
 
 
 if __name__ == "__main__":
